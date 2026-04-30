d2fbbb9ceccf57c5945101b99bd6cdb6559ed675
aysl320250028-netizen <aysl.320250028@ejust.edu.eg>
2026-04-30 15:51:11 +0300

    Update Queue.h
    Initial project structure

--- Queue.h
+++ Queue.h
@@ -1,1 +1,19 @@
+#ifndef QUEUE_H
+#define QUEUE_H
 
+#include "Worker.h"
+
+class Queue {
+private:
+    int front, rear, count, maxSize;
+    Worker* arr;
+
+public:
+    Queue(int size);
+    bool isEmpty();
+    bool isFull();
+    void enqueue(Worker w);
+    Worker dequeue();
+};
+
+#endif
