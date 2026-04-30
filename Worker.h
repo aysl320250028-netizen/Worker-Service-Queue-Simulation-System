de151f9245441a2c49fb597bae4841fef2b2a936
aysl320250028-netizen <aysl.320250028@ejust.edu.eg>
2026-04-30 15:48:07 +0300

    Update Worker.h
    Initial project structure

--- Worker.h
+++ Worker.h
@@ -1,1 +1,14 @@
+#ifndef WORKER_H
+#define WORKER_H
 
+class Worker {
+public:
+    int id;
+    int arrivalTime;
+    int serviceTime;
+    int startTime;
+
+    Worker(int id = 0, int arrival = 0, int service = 0);
+};
+
+#endif
