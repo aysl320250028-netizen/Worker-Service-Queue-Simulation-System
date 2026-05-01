007693e6e5699bb5e6e29b4b8d760c1826862304
Hana Eissa <hanaeissa725@gmail.com>
2026-05-01 18:36:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,46 +1,25 @@
 #include <iostream>
+#include "Queue.h"
 #include "Simulation.h"
 using namespace std;
 int main() {
     //Queue part:
-    Queue queue(5);
-    cout<< " Circular queue " << endl;
-    cout<< "Is the queue empty? " << (queue.isEmpty() ? "Yes" : "No") << endl;
-    cout << "Is the queue full? " << (queue.isFull() ? "Yes" : "No") << endl;
-    
-    cout<< "Adding the following elements:" << endl;
-    queue.enqueue(10);
-    queue.enqueue(20);
-    queue.enqueue(30);
-    queue.enqueue(40);
-    queue.enqueue(50);
-    queue.enqueue(60);
-    
-    queue.display();
-    
-    cout<< "Deleting the following elements:" << endl;
-    queue.dequeue();
-    queue.dequeue();
-    
-    queue.display();
-    
-    cout<< "Adding the following elements to prove circular behaviour:" << endl;
-    queue.enqueue(60);
-    queue.enqueue(70);
-    
-    queue.display();
-    
-    cout<< "Deleting the remaining elements (ending the queue):" << endl;
-     while (!queue.isEmpty()) {
-        queue.dequeue();
-    }
-    cout<<"Attempting to delete from an empty queue:" << endl;
-    queue.dequeue();
-    
-    cout<< "Reinitializing the queue:" << endl;
-    queue.initializeQueue();
-    
-    queue.display();
+   Queue q(5);
+    cout << "Is queue empty? " << (q.isEmpty() ? "Yes" : "No") << endl;
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    q.enqueue(40);
+    q.enqueue(50);
+    q.display();
+    cout << "Is queue full? " << (q.isFull() ? "Yes" : "No") << endl;
+    q.enqueue(60);
+    q.dequeue();
+    q.dequeue();
+    q.display();
+    q.enqueue(60);
+    q.enqueue(70);
+    q.display();
     
     cout << "Queue Simulation System Started..." << endl;
    Simulation sim1(20, 10, 1);
