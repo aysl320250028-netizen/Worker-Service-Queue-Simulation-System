4c33f4014fd93a06a1db0dfda5948f21286de6b4
shahd993ismail <shahd.ismail@ejust.edu.eg>
2026-05-05 00:06:30 +0300

    Update Worker.cpp

--- Worker.cpp
+++ Worker.cpp
@@ -1,11 +1,16 @@
 #include "Worker.h"
+#include <iostream>
+using namespace std;
 
-Worker::Worker(int id, int arrival, int service) {
+Worker::Worker(int id, int arrivalTime, int serviceTime) {
     this->id = id;
-    this->arrivalTime = arrival;
-    this->serviceTime = service;
-    this->startTime = -1;
+    this->arrivalTime = arrivalTime;
+    this->serviceTime = serviceTime;
 }
 
-
+void Worker::display() const {
+    cout << "Worker [ID: " << id
+         << " | Arrival Time: " << arrivalTime
+         << " | Service Time: " << serviceTime << "]" << endl;
+}
 
