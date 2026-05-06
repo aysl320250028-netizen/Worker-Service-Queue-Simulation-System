2bf33448e3c99085a274b872be0d104de168a19f
Aysl Elsharkawy <aysl.320250028@ejust.edu.eg>
2026-05-06 15:20:08 +0300

    Update Simulation.h

--- Simulation.h
+++ Simulation.h
@@ -1,17 +1,49 @@
 #ifndef SIMULATION_H
 #define SIMULATION_H
-
 #include "Queue.h"
 #include "Server.h"
+#include "Worker.h"
 
 class Simulation {
 private:
     int time;
     int maxTime;
-   Queue<Worker> queue;
+
+    Queue<Worker> queue;   // ✅ FIXED
 
     Server* servers;
     int numServers;
+
+    int totalWaitingTime;
+    int servedWorkers;
+    int maxQueueLength;
+
+public:
+    Simulation(int simTime, int queueSize, int numServers);
+    void run();
+};
+
+#endif
+#ifndef SIMULATION_H
+#define SIMULATION_H
+
+#include "Queue.h"
+#include "Server.h"
+#include "Worker.h"
+
+class Simulation {
+private:
+    int time;
+    int maxTime;
+
+    Queue<Worker> queue;   // ✅ FIXED
+
+    Server* servers;
+    int numServers;
+
+    int totalWaitingTime;
+    int servedWorkers;
+    int maxQueueLength;
 
 public:
     Simulation(int simTime, int queueSize, int numServers);
