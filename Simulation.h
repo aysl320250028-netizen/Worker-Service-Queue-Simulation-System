9517406ed69da46efd9d6ec499a44f0e128fb47c
neenassar2007-beep <neenassar2007@gmail.com>
2026-05-07 17:47:14 +0300

    Update Simulation.h

--- Simulation.h
+++ Simulation.h
@@ -24,30 +24,3 @@ public:
 };
 
 #endif
-#ifndef SIMULATION_H
-#define SIMULATION_H
-
-#include "Queue.h"
-#include "Server.h"
-#include "Worker.h"
-
-class Simulation {
-private:
-    int time;
-    int maxTime;
-
-    Queue<Worker> queue;   // ✅ FIXED
-
-    Server* servers;
-    int numServers;
-
-    int totalWaitingTime;
-    int servedWorkers;
-    int maxQueueLength;
-
-public:
-    Simulation(int simTime, int queueSize, int numServers);
-    void run();
-};
-
-#endif
